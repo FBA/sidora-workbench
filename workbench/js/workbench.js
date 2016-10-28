c086687d1032b3f26b82f92f33d7d3dfed6b653b
Simon Fox <sfox@fba.org.uk>
2016-10-28 11:09:15 +0100

    AEDA, FBA initial customisation of Smithsonian code to handle FBA data structures, with autocompletes via Object Linker code (from Nick Bywell's v6 code partially ported to v7)

--- workbench/js/workbench.js
+++ workbench/js/workbench.js
@@ -269,6 +269,88 @@ sidora.concept.LoadContentHelp.Exhibition_view = function(conceptOfInterest){
     }
   });
 }
+/* 
+ * Sets the visibility of the concepts listed in the "add concept" menu - SF 2016/04/05
+ * 
+ * @param {type} conceptOfInterest
+ * 
+ */
+sidora.concept.LoadContentHelp.Concept_view = function (conceptOfInterest) {
+    jQuery.ajax(
+            {
+                dataType: "json",
+                url: Drupal.settings.basePath + 'sidora/info/' + conceptOfInterest + '/getcmodel',
+                success: function (returnedText) {
+                    var conceptlabel = returnedText.conceptlabel;
+                    var Aobjid = "FBAActivity";
+                    var Dobjid = "FBADataset";
+                    /*SFOX Can't work out how to elegantly disable the click, so 
+                     * doing a brute force hide for now 
+                     */
+
+                    switch (conceptlabel) {
+                        case "FBA Project":
+                        {
+                            jQuery("#" + Aobjid).show();
+                            jQuery("#" + Aobjid).children('a').show();
+                            jQuery("#" + Dobjid).hide();
+                            jQuery("#" + Dobjid).children('a').hide();
+                            jQuery("#concept-create").show();
+                            break;
+                        }
+                        case "FBA Dataset":
+                        {
+                            jQuery("#" + Aobjid).hide();
+                            jQuery("#" + Aobjid).children('a').hide();
+                            jQuery("#" + Dobjid).hide();
+                            jQuery("#" + Dobjid).children('a').hide();
+                            jQuery("#concept-create").hide();
+                            break;
+                        }
+                        default:
+                        {
+                            jQuery("#" + Aobjid).show();
+                            jQuery("#" + Aobjid).children('a').show();
+                            jQuery("#" + Dobjid).show();
+                            jQuery("#" + Dobjid).children('a').show();
+                            jQuery("#concept-create").show();
+                        }
+                    }
+                }
+            }
+    );
+}
+/*
+ * Code to choose when to show and hide the Resource list tab
+ * Dataset = show, all other concepts = hide
+ * 
+ * @param {type} conceptOfInterest
+ */
+sidora.concept.LoadContentHelp.ResourceList_view = function (conceptOfInterest) {
+    jQuery.ajax(
+            {
+                dataType: "json",
+                url: Drupal.settings.basePath + 'sidora/info/' + conceptOfInterest + '/getcmodel',
+                success: function (returnedText) {
+                    var conceptlabel = returnedText.conceptlabel;
+                    var objid1 = "ui-id-1"; //Concept Overview tab
+                    var objid3 = "ui-id-3"; //Resource List tab
+                    switch (conceptlabel) {
+                        case "FBA Dataset":
+                        {
+                            jQuery("#" + objid3).show();
+                            break;
+                        }
+                        default:
+                        {
+                            jQuery("#" + objid3).hide();
+                            jQuery("#" + objid1).click(); //Force the first tab to be active
+                        }
+                    }
+                }
+            }
+    );
+}
 /*
  * Loads the concept relationships screen for the concept
  */
@@ -425,7 +507,9 @@ sidora.concept.LoadContent = function(leaveContentIfAlreadyLoaded){
   sidora.concept.LoadContentHelp.Metadata(conceptOfInterest);
   sidora.concept.LoadContentHelp.FullTableReload(conceptOfInterest);
   sidora.concept.loadedContentPid = conceptOfInterest;
+    sidora.concept.LoadContentHelp.Concept_view(conceptOfInterest); //SFOX
   sidora.ReformatPage();
+    sidora.concept.LoadContentHelp.ResourceList_view(conceptOfInterest); //SFOX
 }
 /*
  * Used to see if there is at least a tag in there someplace
@@ -1431,7 +1515,9 @@ sidora.ontology._createSubmenu = function(ontologyChildren){
       else {
        classIcon = '&nbsp;&nbsp;';
       }  
-      toReturn += ("<li title='"+obj.description+"' class=''><a onclick='return false;' href='#'"+model+formName+ontologyId+" class="+classDisabled+">"+key.replace(/ /g, '&nbsp;')+classIcon+"</a>"+childrenHtml+"</li>\n");
+            //SFOX add an id, so options can be greyed out
+            var objid = obj.description.replace(/\s+/g, '');
+            toReturn += ("<li id='" + objid + "' title='" + obj.description + "' class=''><a onclick='return false;' href='#'" + model + formName + ontologyId + " class=" + classDisabled + ">" + key.replace(/ /g, '&nbsp;') + classIcon + "</a>" + childrenHtml + "</li>\n");
     }
   }
   return toReturn;
